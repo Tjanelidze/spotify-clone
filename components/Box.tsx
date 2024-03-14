1472bb32a9d7b5455a997143ea993fa3dcb8bb1c
Tedo <tedojanelidze7@gmail.com>
2024-03-14 15:17:00 +0400

    created sidebar, sidebarItem, Box for sidebar content and library component

---
+++ components/Box.tsx
@@ -0,0 +1,18 @@
+import { twMerge } from "tailwind-merge";
+
+interface BoxProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Box: React.FC<BoxProps> = ({ children, className }) => {
+  return (
+    <div
+      className={twMerge("roundend-lg h-fit w-full bg-neutral-900", className)}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Box;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,55 @@
-import React from 'react';
+"use client";
 
-export default function Sidebar() {
-  return <div>Sidebar</div>;
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
+
+interface SidebarProps {
+  children: React.ReactNode;
 }
+
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathname = usePathname();
+
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathname !== "/search",
+        href: "/home",
+      },
+      {
+        icon: BiSearch,
+        label: "search",
+        active: pathname === "/search",
+        href: "/search",
+      },
+    ],
+    [pathname],
+  );
+  return (
+    <div className="flex h-full">
+      <div className="hidden h-full w-[300px] flex-col gap-y-2 bg-black p-2 md:flex">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="h-full overflow-y-auto">
+          <Library />
+        </Box>
+      </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;

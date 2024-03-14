f735c919ab7e6dc21e2e6efa28f92da6e5e594e6
Tedo <tedojanelidze7@gmail.com>
2024-03-14 23:40:28 +0400

    added header

---
+++ components/Button.tsx
@@ -0,0 +1,14 @@
+import React, { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return <button></button>;
+  },
+);
+
+Button.displayName = "Button";
+
+export default Button;
---
+++ components/Header.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { twMerge } from "tailwind-merge";
+import Button from "./Button";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    // Handle logout in the future
+  };
+
+  return (
+    <div
+      className={twMerge(
+        "h-fit bg-gradient-to-b from-emerald-800 p-6",
+        className,
+      )}
+    >
+      <div className="mb-4 flex w-full items-center justify-between">
+        <div className="hidden items-center gap-x-2 md:flex">
+          <button
+            onClick={() => router.back()}
+            className="flex items-center justify-center rounded-full bg-black transition hover:opacity-75"
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="flex items-center justify-center rounded-full bg-black transition hover:opacity-75"
+          >
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+        <div className="flex items-center gap-x-2 md:hidden">
+          <button className="flex items-center justify-center rounded-full bg-white p-2">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="flex items-center justify-center rounded-full bg-white p-2">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex items-center justify-between gap-x-4">
+          <>
+            <div>
+              <Button>Sign up</Button>
+            </div>
+          </>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

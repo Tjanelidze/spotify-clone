8409fcf0f5b472e9dc783df872facf646937556d
Tedo <tedojanelidze7@gmail.com>
2024-03-20 23:37:30 +0400

    implemented library component and sidebar for showing songs

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,12 +1,19 @@
 "use client";
 
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+
 import useAuthModal from "@/hooks/useAuthModal";
 import useUploadModal from "@/hooks/useUploadModal";
 import { useUser } from "@/hooks/useUser";
-import { AiOutlinePlus } from "react-icons/ai";
-import { TbPlaylist } from "react-icons/tb";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -34,7 +41,11 @@ const Library = () => {
           className="cursor-pointer text-neutral-400 transition hover:text-white"
         />
       </div>
-      <div className="mt-4 flex  flex-col gap-y-2 px-3">List of Songs!</div>
+      <div className="mt-4 flex  flex-col gap-y-2 px-3">
+        {songs.map((item) => (
+          <MediaItem onClick={() => {}} key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   );
 };

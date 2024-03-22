839e13ea39e2b7c8fe1880eea578b477b4dd7cf0
Tedo <tedojanelidze7@gmail.com>
2024-03-22 23:55:35 +0400

    implemeted like button

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LikedButton from "@/components/LikedButton";
 import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
 
@@ -21,7 +22,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
           <div className=" flex-1">
             <MediaItem onClick={() => {}} data={song} />
           </div>
-          {/* TODO: Add Like Button Here */}
+          <LikedButton songId={song.id} />
         </div>
       ))}
     </div>
